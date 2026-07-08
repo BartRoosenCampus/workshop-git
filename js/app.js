1887a1ab193a3c775b902a7bd65ca1da49c0e774
Bart Roosen <84047662+BartRoosenCampus@users.noreply.github.com>
2026-07-08 08:49:44 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const page = {
+    menu_placeholder: document.getElementById('menu_placeholder'),
+    title_placeholder: document.getElementById('title_placeholder'),
+    content_placeholder: document.getElementById('content_placeholder'),
+    nav_links: document.getElementsByClassName('nav_link'),
+}
+
+function attachEventListners() {
+    for (const link of page.nav_links) {
+        link.addEventListener('click', (e) => {
+            console.log(link.dataset.file);
+            e.preventDefault();
+            getContent(link.dataset.file, page.content_placeholder);
+        })
+    }
+}
+
+function getContent(path, placeholder) {
+    return fetch(`components/${path}.html`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Fout bij het laden van het bestand');
+            }
+            return response.text(); // Zet de response om in tekst (HTML)
+        })
+        .then(data => {
+            placeholder.innerHTML = data; // Stop de HTML in de placeholder
+        })
+        .catch(error => {
+            console.error('Er is iets misgegaan:', error);
+        });
+}
+
+getContent("title", page.title_placeholder);
+getContent("home", page.content_placeholder);
+getContent("nav", page.menu_placeholder)
+    .then(data => {
+        attachEventListners();
+    });
